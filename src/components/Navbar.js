5b97a8e0463139cf89c17a24e00c76ee254fa341
Edgarrd11 <edgar.dorador@gmail.com>
2025-03-12 15:30:38 -0600

    React App initialized :)

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='container'>
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand">LoanMGMT</a>
+                <button class="btn btn-outline-success" type="submit">Login</button>
+            </div>
+        </nav>
+    </div>
+  )
+}
---
+++ src/views/HomeView.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+export default function HomeView() {
+// Home view  
+    return (
+    <div className='container'>
+        <Navbar/>
+        <div>HeroSection</div>
+        <div>FooterSection</div>
+    </div>
+  )
+}
